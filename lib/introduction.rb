dcafd2e2cf8c011fd7500dd1a6fb9c74844f9bf2
Sarah Wostenberg <swostenberg@gmail.com>
2021-06-08 00:24:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,2 @@
-#name = "Sarah" 
-#language = "Ruby" 
-
 def introduction(name)
-end 
-
-def introduction_with_language(name, language)
-  puts "Hi! My name is #{name} and I'm learning #{language}"
 end 
